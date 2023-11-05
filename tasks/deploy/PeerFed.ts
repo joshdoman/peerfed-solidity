def2dea453f25a5b8ea20ed365fe6affe681ef40
Josh Doman <joshsdoman@gmail.com>
2023-11-05 18:17:58 -0500

    feat: added PeerFedLibraryExternal to deployment

--- tasks/deploy/PeerFed.ts
+++ tasks/deploy/PeerFed.ts
@@ -2,8 +2,8 @@ import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
-import type { PeerFed } from "../../src/types/contracts";
-import type { PeerFed__factory } from "../../src/types/factories/contracts";
+import type { PeerFed, PeerFedLibraryExternal } from "../../src/types/contracts";
+import type { PeerFed__factory, PeerFedLibraryExternal__factory } from "../../src/types/factories/contracts";
 
 task("deploy:PeerFed").setAction(async function (taskArguments: TaskArguments, { ethers }) {
   const signers: SignerWithAddress[] = await ethers.getSigners();
@@ -15,7 +15,12 @@ task("deploy:PeerFed").setAction(async function (taskArguments: TaskArguments, {
   const token0 = await peerfed.token0();
   const token1 = await peerfed.token1();
 
+  const libraryFactory: PeerFedLibraryExternal__factory = <PeerFedLibraryExternal__factory>await ethers.getContractFactory("PeerFedLibraryExternal");
+  const library: PeerFedLibraryExternal = <PeerFedLibraryExternal>await libraryFactory.connect(owner).deploy();
+  await library.deployed();
+
   console.log("PeerFed deployed to: ", peerfed.address);
   console.log("token0 deployed to: ", token0);
   console.log("token1 deployed to: ", token1);
+  console.log("PeerFedLibraryExternal deployed to: ", library.address);
 });
